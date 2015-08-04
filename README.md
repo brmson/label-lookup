0ae8dc4e5ee68122244a904210040cb248b45222
Nguyen Hoang Long <roman.long@email.cz>
2015-08-04 18:25:04 +0200

    script now returns wiki pageID with the label

--- README.md
+++ README.md
@@ -3,9 +3,18 @@
 Pseudo-fuzzy search script for dbpedia label lookup based on 
 kitt.ai paper (http://aclweb.org/anthology/N/N15/N15-3014.pdf) by Xuchen Yao.
 
-The script currently requires the labels_en.nq file to be in the same directory
+The script currently requires the labels_en.nq and page-ids-en.nq files to be in the same directory
 
 Download it at  http://downloads.dbpedia.org/2015-04/core-i18n/en/labels_en.nq.bz2
+It will create 2 pickle files when run for the first time. The dumping takes ages and might give 
+MemoryErrors on older systems with less than 8GB RAM.
+
 
 Just run the script, wait about 2 minutes and then send requests to 
-"localhost:5000/search/searchedlabel", for example "localhost:5000/search/AlbaniaPeople"
+"localhost:5000/search/searchedlabel"
+for example "localhost:5000/search/AlbaniaPeople"
+Alternatively, it is possible to run it in interactive mode, but you have
+to change "if __name__ == "__main__": web_init()" to 
+"if __name__ == "__main__": interactive()"
+
+It returns a json containing the label and it's wikipedia pageID.
--- labelNameExtractor.py
+++ labelNameExtractor.py
@@ -3,23 +3,79 @@
 
 edit_threshold = 3
 neighbours_to_check = 2 # the checked amount is double, because we look n positions up and n positions down
-labels = []
+labels_and_ids = []
 reversed_labels = []
+labels_filename = "labels_en.nq"
+pageIDs_filename = "page-ids_en.nq"
+
+
 from flask import *
+#import os
+#import cPickle as pickle
+
 app = Flask(__name__)
+
+
 
 #list of tuples, labels[i][0] is the label which we search labels[i][1] is the property name which we return
 def load_labels():
     loaded_labels = []
-    with open("labels_en.nq", "r") as f:
+    with open(labels_filename, "r") as f:
         counter = 0
+        next(f)
         for line in f:
             name = line[len('<http://dbpedia.org/resource/'):line.find('>')]
-            start_index = line.find('\"')+1
-            end_index = line.find('\"', start_index)
-            label = line[start_index:end_index]
+            label_start_index = line.find('\"')+1
+            label_end_index = line.find('\"', label_start_index)
+            label = line[label_start_index:label_end_index]
             loaded_labels.append((label, name))
     return loaded_labels
+
+#XXX code duplication
+#labels[i][0] is the property name and labels[i][1] is the pageID
+def load_IDs():
+    loaded_IDs = []
+    with open(pageIDs_filename, "r") as p:
+        next(p)
+        for line in p:
+            pageID_name = line[len('<http://dbpedia.org/resource/'):line.find('>')]
+            pageID_start_index = line.find('\"') + 1
+            pageID_end_index = line.find('\"', pageID_start_index)
+            pageID = line[pageID_start_index:pageID_end_index]
+            loaded_IDs.append((pageID_name, pageID))
+    return loaded_IDs
+
+#finds a wikipedia ID for each label
+def merge_tuple_lists(label_list, ID_list):
+    result_list = []
+    skipped = 0
+    print str(len(label_list))
+    print str(len(ID_list))
+    counter = 0
+    for x in range(0, len(label_list)):
+        nextID = binary_retrieve(label_list[x][1], ID_list)
+        if(nextID != -1):
+            result_list.append((label_list[x][0], label_list[x][1], nextID))
+        else:
+            skipped = skipped + 1
+    percentage = (skipped/float(len(label_list)))*100
+    print "skipped: " + str(skipped) + ", " + str(percentage) + "% "
+    return result_list
+
+def binary_retrieve(name, ids):
+    lower_bound = 0
+    upper_bound = len(ids) - 1
+    found = False
+    middle = 0
+    while lower_bound <= upper_bound and found == False:
+        middle = (lower_bound + upper_bound) // 2
+        if ids[middle][0] < name:
+            lower_bound = middle + 1
+        elif ids[middle][0] > name:
+            upper_bound = middle - 1
+        else:
+            return ids[middle][1]
+    return -1
 
 def levenshtein(s, t): # XXX this can be done better using numPy
         ''' From Wikipedia article; Iterative with two matrix rows. '''
@@ -43,8 +99,8 @@ def binary_search(name, labels):
     lower_bound = 0
     upper_bound = len(labels)
     middle = 0
-    found = False
     result = set()
+
     while lower_bound <= upper_bound:
         middle = (lower_bound+upper_bound) // 2
         result = result | (check_neighbours(name, labels, middle))
@@ -66,25 +122,47 @@ def check_neighbours(name, labels, index):
     global edit_threshold
     for x in range(start, end):
         if (levenshtein(name, labels[x][0]) <= edit_threshold):
-            res.add(labels[x][1])
+            res.add((labels[x][1], labels[x][2]))
     return res
 
 def web_init():
-    global labels
-    global reversed_labels
-    labels = load_labels()
-    labels.sort(key=lambda x: x[0])
-    reversed_labels = map(lambda x: (x[0][::-1], x[1]), labels)
-    reversed_labels.sort(key=lambda x: x[0])
-    print "init done"
+    init()
     app.run()
+
+def init():
+    global labels_and_ids
+    global reversed_labels
+    if os.path.exists("label_list.pickle"):
+        print "loading labels from pickle"
+        labels_and_ids = pickle.load(open("label_list.pickle", "r"))
+        print len(labels_and_ids)
+    else:
+        print "generating labels from files"
+        labels = load_labels()
+        ids = load_IDs()
+        print "loading done, starting sort"
+        labels.sort(key=lambda x: x[0])
+        ids.sort(key=lambda x: x[0])
+        print "sorting done, merging"
+        labels_and_ids = merge_tuple_lists(labels, ids)
+        del labels
+        del ids
+        print "dumping to pickle"
+        pickle.dump(labels_and_ids, open("label_list.pickle", "w"), protocol=-1)
+    if os.path.exists("reversed_label_list.pickle"):
+        reversed_labels = pickle.load(open("reversed_label_list.pickle", "r"))
+    else:
+        reversed_labels = map(lambda x: (x[0][::-1], x[1], x[2]), labels_and_ids)
+        reversed_labels.sort(key=lambda x: x[0])
+        pickle.dump(reversed_labels, open("reversed_label_list.pickle", "w"), protocol=-1)
+    print "init done"
 
 @app.route('/search/<name>')
 def search(name):
     global labels
     global reversed_labels
     print "searching " + name
-    result = set()
+    result = set
     result = result | binary_search(name, labels)
     result = result | binary_search(name[::-1], reversed_labels)
     result_list = list(result)
@@ -95,10 +173,7 @@ def search(name):
 
 # TOOD: add remote threshold and neighbourcount setting
 def interactive():
-    labels = load_labels()
-    labels.sort(key=lambda x: x[0])
-    reversed_labels = map(lambda x: (x[0][::-1], x[1]), labels)
-    reversed_labels.sort(key=lambda x: x[0])
+    init()
     while (True):
         result = set()
         name = raw_input("lets look for: ")
@@ -114,7 +189,7 @@ def interactive():
             next_num = int(input("current edit threshold is "+edit_threshold+", please input new number"))
             edit_threshold = next_num
             continue
-        result = result | binary_search(name, labels)
+        result = result | binary_search(name, labels_and_ids)
         result = result | (binary_search(name[::-1], reversed_labels))
         print list(result)
         print "now sorted and trimed"
