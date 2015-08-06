5ed9614e4c8d1f1f2264dc69fa77daa4a7131625
Petr Baudis <pasky@ucw.cz>
2015-08-07 00:19:05 +0200

    preprocess: Use maps instead of sorted lists
    This translaes to faster run time as well as smaller memory usage as
    well as much simpler code.

--- liblookup/ntfile.py
+++ liblookup/ntfile.py
@@ -4,7 +4,7 @@ Utilities for loading .nt RDF triple files.
 
 def load_literals(nt_filename, first=0):
     """
-    From an .nt file, produce a list of tuples with the last and first
+    From an .nt file, produce a dict mapping between the first and last
     elements.
 
     We make some strong assumptions:
@@ -12,10 +12,10 @@ def load_literals(nt_filename, first=0):
     * the middle element is always the same predicate (which we ignore)
     * the last element is a literal
 
-    If the first parameter is 0, the first element in the tuple is the
-    name (URL); if it's 1, it's the label.
+    If the @first parameter is 0, the key in the dict is the name (URL);
+    if it's 1, the key is the label.
     """
-    tuples = []
+    mapping = dict()
     with open(nt_filename, "r") as f:
         next(f)  # a comment comes first
         for line in f:
@@ -24,7 +24,7 @@ def load_literals(nt_filename, first=0):
             l_end_index = line.find('\"', l_start_index)
             l = line[l_start_index:l_end_index]
             if first == 0:
-                tuples.append((name, l))
+                mapping[name] = l
             else:
-                tuples.append((l, name))
-    return tuples
+                mapping[l] = name
+    return mapping
--- preprocess.py
+++ preprocess.py
@@ -15,59 +15,24 @@ import sys
 from liblookup import ntfile
 
 
-def binary_retrieve(name, ids):
-    lower_bound = 0
-    upper_bound = len(ids) - 1
-    middle = 0
-    while lower_bound <= upper_bound:
-        middle = (lower_bound + upper_bound) // 2
-        if ids[middle][0] < name:
-            lower_bound = middle + 1
-        elif ids[middle][0] > name:
-            upper_bound = middle - 1
-        else:
-            return ids[middle][1]
-    return -1
-
-
-# finds a wikipedia ID for each label
-# list[x][0] is the label we use to search, list[x][1] is the name to return and list[x][2] is the wikiID
-def merge_tuple_lists(label_list, ID_list):
-    result_list = []
-    skipped = 0
-    print str(len(label_list))
-    print str(len(ID_list))
-    for x in range(0, len(label_list)):
-        nextID = binary_retrieve(label_list[x][1], ID_list)
-        if(nextID != -1):
-            result_list.append((label_list[x][0], label_list[x][1], nextID))
-        else:
-            skipped = skipped + 1
-    percentage = (skipped/float(len(label_list)))*100
-    print "skipped: " + str(skipped) + ", " + str(percentage) + "% "
-    return result_list
-
-
-def save_to_file(list_filename):
+def save_to_file(labels, label_map, id_map, list_filename):
     with open(list_filename, "w") as f:
-        for x in range(0, len(labels_and_ids)):
-            f.write(labels_and_ids[x][0] + "\t" + labels_and_ids[x][1] + "\t" + labels_and_ids[x][2] + "\n")
+        for label in labels:
+            url = label_map[label]
+            pageID = id_map[url]
+            f.write("%s\t%s\t%s\n" % (label, url, pageID))
 
 
 if __name__ == "__main__":
     labels_filename, pageIDs_filename, list_filename = sys.argv[1:]
 
     print "loading labels"
-    labels = ntfile.load_literals(labels_filename, first=1)
+    label_map = ntfile.load_literals(labels_filename, first=1)
     print "loading IDs"
-    ids = ntfile.load_literals(pageIDs_filename)
+    id_map = ntfile.load_literals(pageIDs_filename)
 
-    print "loading done, starting sort"
-    labels.sort(key=lambda x: x[0])
-    ids.sort(key=lambda x: x[0])
-
-    print "sorting done, merging"
-    labels_and_ids = merge_tuple_lists(labels, ids)
+    print "loading done, sorting"
+    labels = sorted(label_map.keys())
 
     print "saving to file"
-    save_to_file(list_filename)
+    save_to_file(labels, label_map, id_map, list_filename)
